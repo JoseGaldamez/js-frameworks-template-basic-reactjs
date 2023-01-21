90dbfc85cbaf6670cc25f88ac31598e8576587c7
José Galdámez <josegaldamez1991@gmail.com>
2023-01-21 15:18:53 -0600

    init project

---
+++ src/App/index.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import reactJSLogo from "../assets/reactjs-logo.png";
+
+export const App = () => {
+  return (
+    <>
+      <h1>Basic React JS</h1>
+      <img className="logo" src={reactJSLogo} alt="Logo React JS" />
+    </>
+  );
+};
